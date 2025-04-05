111cdfe035fc99287035a88b8ff83dd10dc3c5ff
leah-massey <15648177+leah-massey@users.noreply.github.com>
2025-04-05 14:28:08 +0100

    REFACTOR: comments and renaming for clarity

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,64 +1,75 @@
 fun main() {
     println("Welcome to error handling HQ :-)")
-    // example 1
-//    basicDivider(2, 0)
+    // EXAMPLE 1
+//    divider(2, 0)
 
-    // example 2
-//    dividerWithStandardException(2, 0)
+    // EXAMPLE 2
+//    dividerIfElse(2, 0)
 
+    // EXAMPLE 3
+//    try {
+//        dividerThatThrowsException(2, 0)
+//    } catch (e: Exception) {
+//        e.printStackTrace()
+//    }
 
-    try {dividerWithCustomException(2, 0)}
-    catch (e: DivisionByZeroException) {
+    //EXAMPLE 4
+    try {
+        dividerThatThrowsCustomException(2, 0)
+    } catch (e: DivisionExceptionWithMessage) {
         e.printStackTrace()
-        println(e.message)
     }
 
 }
 
+// 1
 // catch generic exception within the method
 // ✅ catches all types of exceptions
 // ✅ immediate feedback
 // ⛔️ catching generic exceptions can make debugging harder and is considered bad practise
 fun divider(a: Int, b: Int): Int {
-    try {
+    return try {
         a / b
     } catch (e: Exception) {
-        println("basic message: you cannot divide by zero")
+        println("you cannot divide by zero")
         e.printStackTrace() // prints the stack trace to the terminal
+        0 // return a safe, default value
     }
-    return a / b
 }
 
+// 2
+// no try catch
+// ⛔️ we don't catch an exception here, we just avoid it occurring - this could make spotting issues we want to be aware of hard.
 fun dividerIfElse(a: Int, b: Int): Int {
-    return if (b != 0 ) {
-        a/b
+    return if (b != 0) {
+        a / b
     } else {
-        0 // not sure this is okay
+        0
     }
 }
 
-
+// 3
 //  catch generic exception and throw custom exception within the method
-fun dividerWithStandardException(a: Int, b: Int): Int {
-    try {
-        a / b
-    } catch (e: Exception) {
-        throw DivisionByZeroException("custom message needed as part of decl")
+fun dividerThatThrowsException(a: Int, b: Int): Int {
+    if (b == 0 || a == 0) {
+        throw Exception()
     }
     return a / b
 }
 
+// 4
 // throw custom exception within the method (catch later, further up the stack)
 //  ✅ separation of concerns - it is up to the caller to handle the error
 //  ✅ exception thrown before attempting division which prevents a redundant operation
 //  ✅ the caller can handle the error where they have more context for handling it appropriately
-fun dividerWithCustomException(a: Int, b: Int): Int {
+fun dividerThatThrowsCustomException(a: Int, b: Int): Int {
     if (b == 0 || a == 0) {
-        throw DivisionByZeroException("this message can be here or you can leave it out")
+        throw DivisionExceptionWithMessage("you cannot divide by zero")
     }
-    return a/b
+    return a / b
 }
 
+class DivisionExceptionWithMessage(message: String) : ArithmeticException(message)
 
 
 // Example stack trace
@@ -70,7 +81,5 @@ fun dividerWithCustomException(a: Int, b: Int): Int {
 //at MainKt.divider(Main.kt:9)
 //at MainKt.main(Main.kt:4)
 //at MainKt.main(Main.kt)
-
-class DivisionByZeroException(message: String) : ArithmeticException(message)
 
 //"This is the DivisionByZeroException custom message: You cannot divide by zero"
